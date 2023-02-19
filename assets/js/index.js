96107475ca12f7ce56fe257e1a8eac757728c07e
amarodolci <amandajanebaptiste@gmail.com>
2023-02-19 13:05:23 -0500

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,11 @@
 console.log("Your index.js file is loaded correctly!");
 
 $(window).scroll(function () {
-    if ( $(this).scrollTop() > 200 && !$('#navBar img').hasClass('open') ) {
-      $('#navBar img').addClass('open');
-      $('#navBar img').slideDown();
+    if ( $(this).scrollTop() > 200 && !$('navBar img').hasClass('open') ) {
+      $('navBar img').addClass('open');
+      $('navBar img').slideDown();
      } else if ( $(this).scrollTop() <= 200 ) {
-      $('#navBar img').removeClass('open');
-      $('#navBar img').slideUp();
+      $('navBar img').removeClass('open');
+      $('navBar img').slideUp();
     }
   });
