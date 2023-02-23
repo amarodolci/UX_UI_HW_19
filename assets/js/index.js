45f5205b8f8cf675a02e82d1d416d219a88cb7ef
amarodolci <amandajanebaptiste@gmail.com>
2023-02-23 08:22:57 -0500

    fuck this

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,3 +9,17 @@ $(body).scroll(function () {
       $('header').slideUp();
     }
   });
+
+const $icon = document.querySelector('.icon');
+const $arrow = document.querySelector('.arrow');
+
+$icon.mouseenter = () => {
+  $arrow.animate([
+    {left: '0'},
+    {left: '10px'},
+    {left: '0'}
+  ],{
+    duration: 700,
+    iterations: Infinity
+  });
+}
